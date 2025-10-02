54cb2468085e70f0a7b56e151fd83e0086f7dcab
Akanksha-Khantwal <akankshakhantwal737@gmail.com>
2025-10-02 17:39:56 +0530

    Time: 32 ms (95.78%), Space: 62.6 MB (55.29%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+void backtrack(int start,int k,int n,vector<int>&current,vector<vector<int>>&result){
+    if(current.size()==k){
+        result.push_back(current);
+        return;
+    }
+
+    for(int i=start;i<=n;i++){
+        current.push_back(i);
+        backtrack(i+1,k,n,current,result);
+        current.pop_back();
+    }
+}
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>>result;
+        vector<int>current;
+        backtrack(1,k,n,current,result);
+        return result;
+    }
+};
